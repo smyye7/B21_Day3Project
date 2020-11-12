0cc3c54c1c30d413eee8e04844a8925e05e367a4
smyye7 <74027064+smyye7@users.noreply.github.com>
2020-11-12 07:45:55 -0800

    b21 is dedicated

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Git Last Day");
-        // B21
+        // B21 is exited!!!
+        // B21 is dedicated
     }
 }
